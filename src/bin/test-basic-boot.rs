39b32fdd9c36b2c12cd81badc3c39e589cffb5a1
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2019-01-15 20:45:17 +0000

    Added integration tests

---
+++ src/bin/test-basic-boot.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+use core::panic::PanicInfo;
+use rustos::{exit_qemu, serial_println};
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_println!("ok");
+
+    unsafe { exit_qemu(); }
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("failed");
+
+    serial_println!("{}", info);
+
+    unsafe { exit_qemu(); }
+
+    loop {}
+}
