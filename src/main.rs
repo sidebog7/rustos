6e13e249fb55837223394ce6b1699e03fa066999
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2019-01-15 12:24:56 +0000

    Setup for testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 
+#[cfg(not(test))] // only compile when the test flag is not set
 #[no_mangle]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,6 +16,7 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
