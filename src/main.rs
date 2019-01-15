6594790d5e56ceba853d7d3ca376438c99fce0d4
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2019-01-15 17:19:52 +0000

    Added serial based tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(unused_imports))]
 
-mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+mod vga_buffer;
+mod serial;
 
 
 #[cfg(not(test))] // only compile when the test flag is not set
@@ -21,6 +22,16 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
+    serial_println!("Hello Host{}", "!");
 
+    unsafe { exit_qemu(); }
+    
     loop {}
+}
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
 }
